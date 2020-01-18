4581258c78bc5d679480f270eb2b97ad9c07c805
hks2002 <56649783@qq.com>
2020-01-18 16:45:18 +0800

    add date/time to pageheader

--- web/src/components/PageHeader.vue
+++ web/src/components/PageHeader.vue
@@ -26,21 +26,39 @@
                color="primary"
                text-color="white"
                class="q-mr-sm" />
-          <q-btn flat
-                 dense
-                 to="/About"
-                 label="About"
-                 color="primary"
-                 text-color="white"
-                 class="q-mr-sm" />
+        <q-btn flat
+               dense
+               to="/About"
+               label="About"
+               color="primary"
+               text-color="white"
+               class="q-mr-sm" />
       </q-toolbar-title>
+      <q-space />
+      <div>{{noformattedString}}</div>
     </q-toolbar>
+
   </q-header>
 </template>
 
 <script>
+import { date } from 'quasar'
 export default {
-  name: 'PageHeader'
+  name: 'PageHeader',
+  data () {
+    return {
+      noformattedString: this.formatedNow()
+    }
+  },
+  methods: {
+    formatedNow () {
+      let timeStamp = Date.now()
+      this.noformattedString = date.formatDate(timeStamp, 'YYYY-MM-DD ddd HH:mm:ss')
+    }
+  },
+  mounted () {
+    setInterval(this.formatedNow, 1000)
+  }
 }
 </script>
 
