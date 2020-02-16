bdd9e877423f4ab585bcb4a87464106cd00650cc
hks2002 <56649783@qq.com>
2020-02-17 03:10:01 +0800

    fixed app name

--- server/src/main/java/sageassistant/SrvApplication.java
+++ server/src/main/java/sageassistant/SrvApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Srv1Application {
+public class SrvApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Srv1Application.class, args);
+		SpringApplication.run(SrvApplication.class, args);
 	}
 
 }
