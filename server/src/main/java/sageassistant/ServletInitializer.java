0b107f5b48711355c3000c64a71d922a65950a67
hks2002 <56649783@qq.com>
2020-02-17 03:08:52 +0800

    change deploy way to war

--- server/src/main/java/sageassistant/ServletInitializer.java
+++ server/src/main/java/sageassistant/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Srv1Application.class);
+		return application.sources(SrvApplication.class);
 	}
 
 }
