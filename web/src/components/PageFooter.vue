401b517fb7a6b0fc1c978f5e2fb5f6e358914a85
hks2002 <56649783@qq.com>
2020-02-19 23:53:19 +0800

    fixed server infomation display

--- web/src/components/PageFooter.vue
+++ web/src/components/PageFooter.vue
@@ -25,7 +25,7 @@ export default {
   },
   methods: {
     loadData () {
-      this.$axios.get('srvInfo')
+      this.$axios.get('/Data/SrvInfo')
         .then((response) => {
           this.springBootVersion = response.data.springBootVersion
         })
--- web/src/pages/About.vue
+++ web/src/pages/About.vue
@@ -39,7 +39,7 @@ export default {
   },
   methods: {
     loadData () {
-      this.$axios.get('Data/SrvInfo')
+      this.$axios.get('/Data/SrvInfo')
         .then((response) => {
           this.srvName = response.data.name
           this.srvVersion = response.data.version
