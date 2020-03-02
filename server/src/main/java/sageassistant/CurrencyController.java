0d07ac81d6bd909b1906c7c29e2cda19cbbf029e
hks2002 <56649783@qq.com>
2020-03-02 13:56:15 +0800

    add default currency

--- server/src/main/java/sageassistant/CurrencyController.java
+++ server/src/main/java/sageassistant/CurrencyController.java
@@ -2,6 +2,7 @@ package sageassistant;
 
 import java.io.IOException;
 import java.text.NumberFormat;
+import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.http.HttpEntity;
@@ -29,6 +30,9 @@ import net.sf.json.JSONArray;
 @RestController
 public class CurrencyController {
 	private static final Logger log = LoggerFactory.getLogger(CurrencyController.class);
+
+	private HashMap<String, String> dafaultRate = new HashMap<String, String>();
+
 	/**
 	 * google guava cache
 	 */
@@ -38,6 +42,18 @@ public class CurrencyController {
 			return getFromUrl(key);
 		}
 	});
+
+	CurrencyController() {
+		dafaultRate.put("USDRMB", "7.0");
+		dafaultRate.put("EURRMB", "7.5");
+		dafaultRate.put("HKDRMB", "0.9");
+		dafaultRate.put("GBPRMB", "9.0");
+		dafaultRate.put("AUDRMB", "4.6");
+		dafaultRate.put("JPYRMB", "0.06");
+		dafaultRate.put("SDGRMB", "5.0");
+		dafaultRate.put("AEDRMB", "0.5");
+		dafaultRate.put("CADRMB", "5.2");
+	}
 
 	private String doGet(String path) {
 		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
@@ -72,17 +88,17 @@ public class CurrencyController {
 	}
 
 	private String getFromUrl(String key) {
-		log.info("key:" + key);
+		log.debug("key:" + key);
 		String CurrencySour = key.substring(0, 3);
 		String CurrencyDest = key.substring(3, 6);
-		String Date = key.substring(6,16);
+		String Date = key.substring(6, 16);
 
 		double rate = 0;
 
 		if (CurrencyDest.equals("RMB")) { // get from 'State Administration of Foreign Exchange'
-			String responseText = doGet("http://www.safe.gov.cn/AppStructured/hlw/ENJsonRmb.do?date="+Date);
+			String responseText = doGet("http://www.safe.gov.cn/AppStructured/hlw/ENJsonRmb.do?date=" + Date);
 			JSONArray jsonARRAY = JSONArray.fromObject(responseText);
-			
+
 			for (Object obj : jsonARRAY) {
 				JSONArray jsonArray = JSONArray.fromObject(obj);
 				if (jsonArray.getString(1).equals(CurrencySour)) {
@@ -98,6 +114,10 @@ public class CurrencyController {
 		NumberFormat nf = NumberFormat.getInstance();
 		nf.setGroupingUsed(false);
 		String rateStr = nf.format(rate);
+
+		if (rateStr.equals("0")) {
+			rateStr = dafaultRate.get(CurrencySour + CurrencyDest);
+		}
 		return rateStr;
 	}
 
