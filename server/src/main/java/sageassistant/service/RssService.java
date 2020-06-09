696e545b9e933244d7a1c8708ad800621bbb3433
hks2002 <56649783@qq.com>
2020-06-09 13:33:37 +0800

    add date range for tobedelivery and tobereceive rss service

--- server/src/main/java/sageassistant/service/RssService.java
+++ server/src/main/java/sageassistant/service/RssService.java
@@ -2,6 +2,7 @@ package sageassistant.service;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,7 +66,7 @@ public class RssService {
 
 	}
 
-	public String checkTobeDelivery(String site) {
+	public String checkTobeDelivery(String site, Integer range) {
 		List<TobeDelivery> list = statusService.findTobeDeliveryBySite(site, 0);
 
 		String content = "<table border=\"2\" style=\"border-collapse:collapse;\">";
@@ -80,10 +81,24 @@ public class RssService {
 		content += "<th>OverDueDays</th>";
 		content += "</tr>";
 
-		for (TobeDelivery o : list) {
-			long diff = Utils.dateDiff(o.getRequestDate(), new Date());
-			if (diff > 0) {
-				content += "<tr style=\"background-color:red;\">";
+		Date now = new Date();
+		Integer Nrange=range*-1;
+		Integer halfNrange=Nrange/2;
+		Integer quarterNrange=Nrange/4;
+		
+		for (Iterator<TobeDelivery> iter = list.iterator(); iter.hasNext();) {
+			TobeDelivery o = iter.next();
+			long diff = Utils.dateDiff(o.getRequestDate(), now);
+			
+			if (diff>=Nrange) {
+				if (diff >= Nrange && diff < halfNrange) {
+					content += "<tr>";
+				}else if (diff >= halfNrange && diff <quarterNrange) {
+					content += "<tr style=\"background-color:yellow;\">";
+				}else if (diff >= quarterNrange) {
+					content += "<tr style=\"background-color:red;\">";
+				}
+				
 				content += "<td>" + o.getProjectNO() + "</td>";
 				content += "<td>" + o.getPN() + "</td>";
 				content += "<td>" + o.getDescription() + "</td>";
@@ -91,20 +106,12 @@ public class RssService {
 				content += "<td>" + fmt.format(o.getOrderDate()) + "</td>";
 				content += "<td>" + fmt.format(o.getRequestDate()) + "</td>";
 				content += "<td>" + diff + "</td>";
-				content += "</tr>";
-			}
-			if (diff < 2) {
-				content += "<tr style=\"background-color:yellow;\">";
-				content += "<td>" + o.getProjectNO() + "</td>";
-				content += "<td>" + o.getPN() + "</td>";
-				content += "<td>" + o.getDescription() + "</td>";
-				content += "<td>" + o.getQty() + "</td>";
-				content += "<td>" + fmt.format(o.getOrderDate()) + "</td>";
-				content += "<td>" + fmt.format(o.getRequestDate()) + "</td>";
-				content += "<td></td>";
-				content += "</tr>";
+				content += "</tr>";				
+			} else {
+				iter.remove();
 			}
 		}
+
 		content += "</table>";
 
 		if (list.size() > 0) {
@@ -114,7 +121,7 @@ public class RssService {
 		}
 	}
 
-	public String checkTobeReceive(String site) {
+	public String checkTobeReceive(String site, Integer range) {
 		List<TobeReceive> list = statusService.findTobeReceiveBySite(site, 0);
 
 		String content = "<table border=\"2\" style=\"border-collapse:collapse;\">";
@@ -131,12 +138,25 @@ public class RssService {
 		content += "<th>ExpectDate</th>";
 		content += "<th>OverDueDays</th>";
 		content += "</tr>";
-
-		for (TobeReceive o : list) {
-			long diff = Utils.dateDiff(o.getExpectDate(), new Date());
-
-			if (diff > 0) {
-				content += "<tr style=\"background-color:red;\">";
+		
+		Date now = new Date();
+		Integer Nrange=range*-1;
+		Integer halfNrange=Nrange/2;
+		Integer quarterNrange=Nrange/4;
+		
+		for (Iterator<TobeReceive> iter = list.iterator(); iter.hasNext();) {
+			TobeReceive o = iter.next();
+			long diff = Utils.dateDiff(o.getExpectDate(), now);
+			
+			if (diff>=Nrange) {
+				if (diff >= Nrange && diff < halfNrange) {
+					content += "<tr>";
+				}else if (diff >= halfNrange && diff <quarterNrange) {
+					content += "<tr style=\"background-color:yellow;\">";
+				} else if (diff >= quarterNrange) {
+					content += "<tr style=\"background-color:red;\">";
+				}
+				
 				content += "<td>" + o.getPurchaseNO() + "</td>";
 				content += "<td>" + o.getLine() + "</td>";
 				content += "<td>" + o.getProjectNO() + "</td>";
@@ -147,23 +167,12 @@ public class RssService {
 				content += "<td>" + fmt.format(o.getAckDate()) + "</td>";
 				content += "<td>" + fmt.format(o.getExpectDate()) + "</td>";
 				content += "<td>" + diff + "</td>";
-				content += "</tr>";
-			}
-			if (diff < 2) {
-				content += "<tr style=\"background-color:red;\">";
-				content += "<td>" + o.getPurchaseNO() + "</td>";
-				content += "<td>" + o.getLine() + "</td>";
-				content += "<td>" + o.getProjectNO() + "</td>";
-				content += "<td>" + o.getPN() + "</td>";
-				content += "<td>" + o.getDescription() + "</td>";
-				content += "<td>" + o.getQty() + "</td>";
-				content += "<td>" + fmt.format(o.getOrderDate()) + "</td>";
-				content += "<td>" + fmt.format(o.getAckDate()) + "</td>";
-				content += "<td>" + fmt.format(o.getExpectDate()) + "</td>";
-				content += "<td></td>";
-				content += "</tr>";
+				content += "</tr>";		
+			} else {
+				iter.remove();
 			}
 		}
+		
 		content += "</table>";
 
 		if (list.size() > 0) {
@@ -195,35 +204,25 @@ public class RssService {
 		for (TobePurchaseBom o : list) {
 			long diff = Utils.dateDiff(o.getCreateDate(), new Date());
 
-			if (diff > 7) {
+			if (diff >= 14) {
 				content += "<tr style=\"background-color:red;\">";
-				content += "<td>" + o.getProjectNO() + "</td>";
-				content += "<td>" + o.getForPN() + "</td>";
-				content += "<td>" + o.getWorkOrderNO() + "</td>";
-				content += "<td>" + o.getBomSeq() + "</td>";
-				content += "<td>" + o.getPN() + "</td>";
-				content += "<td>" + o.getDescription() + "</td>";
-				content += "<td>" + o.getQty() + "</td>";
-				content += "<td>" + o.getShortQty() + "</td>";
-				content += "<td>" + o.getAllQty() + "</td>";
-				content += "<td>" + fmt.format(o.getCreateDate()) + "</td>";
-				content += "<td>" + diff + "</td>";
-				content += "</tr>";
-			} else if (diff > 3) {
-				content += "<tr style=\"background-color:red;\">";
-				content += "<td>" + o.getProjectNO() + "</td>";
-				content += "<td>" + o.getForPN() + "</td>";
-				content += "<td>" + o.getWorkOrderNO() + "</td>";
-				content += "<td>" + o.getBomSeq() + "</td>";
-				content += "<td>" + o.getPN() + "</td>";
-				content += "<td>" + o.getDescription() + "</td>";
-				content += "<td>" + o.getQty() + "</td>";
-				content += "<td>" + o.getShortQty() + "</td>";
-				content += "<td>" + o.getAllQty() + "</td>";
-				content += "<td>" + fmt.format(o.getCreateDate()) + "</td>";
-				content += "<td>" + diff + "</td>";
-				content += "</tr>";
+			} else if (diff >= 7) {
+				content += "<tr style=\"background-color:yellow;\">";
+			} else {
+				content += "<tr>";
 			}
+			content += "<td>" + o.getProjectNO() + "</td>";
+			content += "<td>" + o.getForPN() + "</td>";
+			content += "<td>" + o.getWorkOrderNO() + "</td>";
+			content += "<td>" + o.getBomSeq() + "</td>";
+			content += "<td>" + o.getPN() + "</td>";
+			content += "<td>" + o.getDescription() + "</td>";
+			content += "<td>" + o.getQty() + "</td>";
+			content += "<td>" + o.getShortQty() + "</td>";
+			content += "<td>" + o.getAllQty() + "</td>";
+			content += "<td>" + fmt.format(o.getCreateDate()) + "</td>";
+			content += "<td>" + diff + "</td>";
+			content += "</tr>";
 		}
 		content += "</table>";
 
@@ -255,35 +254,25 @@ public class RssService {
 		for (TobeDealWithOrderLine o : list) {
 			long diff = Utils.dateDiff(o.getOrderDate(), new Date());
 
-			if (diff > 7) {
+			if (diff >= 14) {
 				content += "<tr style=\"background-color:red;\">";
-				content += "<td>" + o.getProjectNO() + "</td>";
-				content += "<td>" + o.getOrderType() + "</td>";
-				content += "<td>" + o.getPN() + "</td>";
-				content += "<td>" + o.getDescription() + "</td>";
-				content += "<td>" + o.getQty() + "</td>";
-				content += "<td>" + o.getUnit() + "</td>";
-				content += "<td>" + o.getCustomerCode() + "</td>";
-				content += "<td>" + o.getCustomerName() + "</td>";
-				content += "<td>" + fmt.format(o.getOrderDate()) + "</td>";
-				content += "<td>" + fmt.format(o.getDemandDate()) + "</td>";
-				content += "<td>" + diff + "</td>";
-				content += "</tr>";
-			} else if (diff > 3) {
-				content += "<tr style=\"background-color:red;\">";
-				content += "<td>" + o.getProjectNO() + "</td>";
-				content += "<td>" + o.getOrderType() + "</td>";
-				content += "<td>" + o.getPN() + "</td>";
-				content += "<td>" + o.getDescription() + "</td>";
-				content += "<td>" + o.getQty() + "</td>";
-				content += "<td>" + o.getUnit() + "</td>";
-				content += "<td>" + o.getCustomerCode() + "</td>";
-				content += "<td>" + o.getCustomerName() + "</td>";
-				content += "<td>" + fmt.format(o.getOrderDate()) + "</td>";
-				content += "<td>" + fmt.format(o.getDemandDate()) + "</td>";
-				content += "<td>" + diff + "</td>";
-				content += "</tr>";
+			} else if (diff >= 7) {
+				content += "<tr style=\"background-color:yellow;\">";
+			} else {
+				content += "<tr>";
 			}
+			content += "<td>" + o.getProjectNO() + "</td>";
+			content += "<td>" + o.getOrderType() + "</td>";
+			content += "<td>" + o.getPN() + "</td>";
+			content += "<td>" + o.getDescription() + "</td>";
+			content += "<td>" + o.getQty() + "</td>";
+			content += "<td>" + o.getUnit() + "</td>";
+			content += "<td>" + o.getCustomerCode() + "</td>";
+			content += "<td>" + o.getCustomerName() + "</td>";
+			content += "<td>" + fmt.format(o.getOrderDate()) + "</td>";
+			content += "<td>" + fmt.format(o.getDemandDate()) + "</td>";
+			content += "<td>" + diff + "</td>";
+			content += "</tr>";
 		}
 		content += "</table>";
 
