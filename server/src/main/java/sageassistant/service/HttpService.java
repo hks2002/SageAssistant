39ed72ac5b50702ec390ca87d53f051b3280f15e
hks2002 <56649783@qq.com>
2021-06-28 15:46:43 +0800

    use httpconnect instead fo httpclient

--- server/src/main/java/sageassistant/service/HttpService.java
+++ server/src/main/java/sageassistant/service/HttpService.java
@@ -1,23 +1,20 @@
 package sageassistant.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.ParseException;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
@@ -26,51 +23,71 @@ import org.springframework.stereotype.Service;
 public class HttpService {
 	private static final Logger log = LogManager.getLogger();
 
-	private static CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-
 	public static String getRequestResult(String path, String method, String postData) {
-		CloseableHttpResponse response = null;
-
+		HttpURLConnection connection = null;
+		BufferedReader reader = null;
 		try {
-			switch (method.toLowerCase()) {
-			case "get": {
-				HttpGet httpGet = new HttpGet(path);
-				response = httpClient.execute(httpGet);
+			URL url = new URL(path);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setConnectTimeout(8000);
+			connection.setReadTimeout(8000);
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+
+			switch (method.toUpperCase()) {
+			case "GET": {
+				connection.setRequestMethod("GET");
 				break;
 			}
-			case "post": {
-				HttpPost httpPost = new HttpPost(path);
-				httpPost.setHeader("accept", "application/json");
-				httpPost.setHeader("content-type", "application/json;charset=utf-8");
-				HttpEntity entity = new StringEntity(postData);
-				httpPost.setEntity(entity);
-				response = httpClient.execute(httpPost);
+			case "POST": {
+				connection.setRequestMethod("POST");
+				connection.setDoOutput(true);
+				connection.setDoInput(true);
+
+				connection.setRequestProperty("accept", "application/json");
+				connection.setRequestProperty("content-type", "application/json;charset=utf-8");
+
+				OutputStream os = null;
+				os = connection.getOutputStream();
+				if (postData != null && postData.length() > 0) {
+					os.write(postData.getBytes());
+					os.flush();
+					os.close();
+				}
 				break;
 			}
 			default: {
-				HttpGet httpGet = new HttpGet(path);
-				response = httpClient.execute(httpGet);
+				connection.setRequestMethod("GET");
 			}
 			}
 
-			return EntityUtils.toString(response.getEntity());
+			if (connection.getResponseCode() == 200) {
+				InputStream in = connection.getInputStream();
 
-		} catch (ClientProtocolException e) {
-			log.error(e.getMessage());
-		} catch (ParseException e) {
-			log.error(e.getMessage());
-		} catch (IOException e) {
+				reader = new BufferedReader(new InputStreamReader(in));
+				StringBuilder response = new StringBuilder();
+				String line;
+				while ((line = reader.readLine()) != null) {
+					response.append(line);
+				}
+
+				return response.toString();
+			} else {
+				return "";
+			}
+
+		} catch (Exception e) {
 			log.error(e.getMessage());
 		} finally {
-			try {
-				if (httpClient != null) {
-					httpClient.close();
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					log.error(e.getMessage());
 				}
-				if (response != null) {
-					response.close();
-				}
-			} catch (IOException e) {
-				log.error(e.getMessage());
+			}
+			if (connection != null) {
+				connection.disconnect();
 			}
 		}
 
@@ -78,94 +95,88 @@ public class HttpService {
 	}
 
 	public void doRequest(HttpServletRequest request, HttpServletResponse responseToClinet) {
-		CloseableHttpResponse response = null;
 
 		String method = request.getMethod();
 		String path = request.getRequestURL().toString();
 		Enumeration<String> headers = request.getHeaderNames();
 
+		HttpURLConnection connection = null;
+		BufferedReader reader = null;
 		try {
-			String data = null;
+			URL url = new URL(path);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setConnectTimeout(8000);
+			connection.setReadTimeout(8000);
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
 
-			switch (method.toLowerCase()) {
-			case "get": {
-				HttpGet httpGet = new HttpGet(path);
-
-				while (headers.hasMoreElements()) {
-					String name = headers.nextElement();
-					String value = request.getHeader(name);
-					httpGet.setHeader(name, value);
-				}
-				response = httpClient.execute(httpGet);
+			switch (method.toUpperCase()) {
+			case "GET": {
+				connection.setRequestMethod("GET");
 				break;
 			}
-			case "post": {
-				HttpPost httpPost = new HttpPost(path);
+			case "POST": {
+				connection.setRequestMethod("POST");
+				connection.setDoOutput(true);
+				connection.setDoInput(true);
+
+				connection.setRequestProperty("accept", "application/json");
+				connection.setRequestProperty("content-type", "application/json;charset=utf-8");
 
 				while (headers.hasMoreElements()) {
 					String name = headers.nextElement();
 					String value = request.getHeader(name);
-					httpPost.setHeader(name, value);
+					connection.setRequestProperty(name, value);
 				}
 
-				httpPost.setHeader("accept", "application/json");
-				httpPost.setHeader("content-type", "application/json;charset=utf-8");
+				OutputStream os = null;
+				os = connection.getOutputStream();
+				String data = null;
 				data = request.getInputStream().toString();
-
-				HttpEntity entity = new StringEntity(data);
-				httpPost.setEntity(entity);
-				response = httpClient.execute(httpPost);
-				break;
-			}
-			case "put": {
-				HttpPut httpPut = new HttpPut(path);
-
-				while (headers.hasMoreElements()) {
-					String name = headers.nextElement();
-					String value = request.getHeader(name);
-					httpPut.setHeader(name, value);
-				}
-
-				httpPut.setHeader("accept", "application/json");
-				httpPut.setHeader("content-type", "application/json;charset=utf-8");
-
-				data = request.getInputStream().toString();
-				HttpEntity entity = new StringEntity(data);
-				httpPut.setEntity(entity);
-				response = httpClient.execute(httpPut);
+				os.write(data.getBytes());
+				os.flush();
+				os.close();
 				break;
 			}
 			default: {
-
+				connection.setRequestMethod("GET");
 			}
 			}
 
-			responseToClinet.setStatus(response.getStatusLine().getStatusCode());
-			Header headersToClient[] = response.getAllHeaders();
-			for (int i = 0; i < headersToClient.length; i++) {
-				responseToClinet.setHeader(headersToClient[i].getName(), headersToClient[i].getValue());
+			responseToClinet.setStatus(connection.getResponseCode());
+			Map<String, List<String>> map = connection.getHeaderFields();
+
+			for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+				responseToClinet.setHeader(entry.getKey(), entry.getValue().toString());
 			}
+
+			InputStream in = connection.getInputStream();
+
+			reader = new BufferedReader(new InputStreamReader(in));
+			StringBuilder response = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				response.append(line);
+			}
+			
 			PrintWriter wr = responseToClinet.getWriter();
-			wr.write(response.getEntity().toString());
+			wr.write(response.toString());
 
-		} catch (ClientProtocolException e) {
-			log.error(e.getMessage());
-		} catch (ParseException e) {
-			log.error(e.getMessage());
-		} catch (IOException e) {
+		} catch (Exception e) {
 			log.error(e.getMessage());
 		} finally {
-			try {
-				if (httpClient != null) {
-					httpClient.close();
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					log.error(e.getMessage());
 				}
-				if (response != null) {
-					response.close();
-				}
-			} catch (IOException e) {
-				log.error(e.getMessage());
+			}
+			if (connection != null) {
+				connection.disconnect();
 			}
 		}
+
 	}
 
 }
