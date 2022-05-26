bd5ef1ac72c9da05d36f861dac0271cc39bd4c5f
hks2002 <56649783@qq.com>
2022-05-26 15:40:22 +0800

    update boot files

--- web2/src/boot/ebus.js
+++ web2/src/boot/ebus.js
@@ -1,4 +1,7 @@
 import mitt from 'mitt'
+
+console.debug('\u001b[35m' + '[Boot] ', 'Ebus')
+
 const ebus = mitt()
 
 // using it: import {ebus} from 'boot/ebus'
--- web2/src/boot/i18n.js
+++ web2/src/boot/i18n.js
@@ -1,25 +1,53 @@
+import messages from '@/i18n/index'
+import { Cookies, Quasar } from 'quasar'
 import { boot } from 'quasar/wrappers'
 import { createI18n } from 'vue-i18n'
-import messages from 'src/i18n'
 
-const i18n = createI18n({
-  locale: 'en-US',
+const langIso = Cookies.get('locale') || 'en-US'
+console.debug('\u001b[35m' + '[Boot] ', 'i18n', langIso)
+
+const i18nCfg = {
+  locale: langIso,
   messages,
   silentTranslationWarn: true,
   silentFallbackWarn: true
-})
+}
+const i18n = createI18n(i18nCfg)
 
-export default boot(({ app }) => {
-  // Set i18n instance on app
-  app.use(i18n)
-})
+/**
+ * Use i18n outside of a Vue file:
+ *
+ * import { i18n } from '/src/boot/i18n'
+ * const { t } = i18n.global
+ * t('Sample Text')
+ *
+ *
+ * Use i18n inside of a Vue file in template:
+ *
+ * {{ $t('Sample Text') }}
+ *
+ *
+ * Use i18n inside of a vue file in script:
+ *
+ * import { useI18n } from 'vue-i18n'
+ * const { t } = useI18n()
+ * t('Sample Text')
+ */
 
 export { i18n }
 
-// In vue template:
-// derectly using $t
-//
-// In script setup():
-// import { useI18n } from 'vue-i18n'
-// const { t } = useI18n()
-// t('xxx')
+export default boot(({ app } /* { app, router, ... } */) => {
+  // Set i18n instance on app
+  app.use(i18n)
+
+  // Set Quasar lang pack
+  Promise.resolve(require('quasar/lang/' + langIso))
+    .then((lang) => {
+      Quasar.lang.set(lang.default)
+    })
+    .catch((err) => {
+      // Requested Quasar Language Pack does not exist,
+      // let's not break the app, so catching error
+      console.debug(err)
+    })
+})
