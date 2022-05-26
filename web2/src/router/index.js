138ba957445ab2c5da22173a9f416ce4c631e8e9
hks2002 <56649783@qq.com>
2022-05-26 15:54:46 +0800

    add route guard

--- web2/src/router/index.js
+++ web2/src/router/index.js
@@ -1,7 +1,13 @@
+import { getToken } from '@/assets/storage'
+import { usePagesStore } from '@/stores/pageManager'
 import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+import {
+  createMemoryHistory,
+  createRouter,
+  createWebHashHistory,
+  createWebHistory
+} from 'vue-router'
 import routes from './routes'
-import { getToken } from 'assets/storage'
 
 /*
  * If not building with SSR mode, you can
@@ -15,7 +21,9 @@ import { getToken } from 'assets/storage'
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory
+    : process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -24,20 +32,46 @@ export default route(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
+    history: createHistory(
+      process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE
+    )
   })
+
+  const pagesStore = usePagesStore()
 
   Router.beforeEach((to, from, next) => {
     // if page is Login, allowed it always.
     if (to.path === '/Login') return next()
-    else {
-      if (!process.env.DEV && !getToken()) {
-        return next('/Login')
-      } else {
-        return next()
-      }
-    }
-  })
+    // if not get login token, redirect to login
+    if (!process.env.DEV && !getToken()) return next('/Login')
 
-  return Router
+    // forward, backward doesn't have name, get name from matched array
+    const idx = to.matched.findIndex((r) => r.name === to.name)
+
+    if (idx >= 0) {
+      const name = to.matched[idx].name || to.matched[0].name
+
+      const query = to.query
+      const params = to.params
+
+      const queryStr = JSON.stringify(query).slice(1, -1)
+      const paramsStr = JSON.stringify(params).slice(1, -1)
+      const nameWithParams = name + queryStr + paramsStr
+
+      pagesStore.hasPage(nameWithParams)
+        ? pagesStore.setActivePage(nameWithParams)
+        : pagesStore.addPage({
+            id: nameWithParams,
+            name: name,
+            query: query,
+            params: params
+          })
+      // default
+      next()
+    } else {
+      console.error('Router Error:', to)
+    }
+
+    return Router
+  })
 })
