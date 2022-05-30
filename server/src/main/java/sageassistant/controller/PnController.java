3646db373ba2a5a03292fc9f11f3703d2f4185ff
hks2002 <56649783@qq.com>
2022-05-30 15:19:56 +0800

    feat: change for new web project

--- server/src/main/java/sageassistant/controller/PnController.java
+++ server/src/main/java/sageassistant/controller/PnController.java
@@ -17,7 +17,7 @@ public class PnController {
 	private PnService pnService;
 
 	@GetMapping("/Data/PNHelper")
-	public String getPNs(@RequestParam(value = "PnOrPnRoot", required = false, defaultValue = "%%") String pnOrPnRoot,
+	public String getPNs(@RequestParam(value = "PN", required = false, defaultValue = "%%") String pnOrPnRoot,
 			@RequestParam(value = "Count", required = false, defaultValue = "200") Integer count) {
 		return Utils.listToString(pnService.findPnByStartWith(pnOrPnRoot, count));
 	}
